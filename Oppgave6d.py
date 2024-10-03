76bd90188d7dbde7727f3db934a87f980a40fd25
Huferu <123447191+Huferu@users.noreply.github.com>
2024-10-03 16:26:46 +0200

    Update Oppgave6d.py
    Lagt til noen tester i skriptet

--- Oppgave6d.py
+++ Oppgave6d.py
@@ -1,8 +1,9 @@
-def LagListe(filnavn:str, antallLister:int):
+def LagListe(filnavn:str, antallLister:int = 1):
     try:
         fil = open(filnavn, "r")
     except FileNotFoundError():
         print("Fant ikke fil")
+        return[]
     except Exception as e:
         print(e)
         return
@@ -14,7 +15,22 @@ def LagListe(filnavn:str, antallLister:int):
             lister[i].append(kolonne[i])
     return lister
 
+if(__name__ == '__main__'):
+    import matplotlib.pyplot as plt
+    from datetime import datetime
+    filNavn1 = "datafiler/temperatur_trykk_met_samme_rune_time_datasett.csv.txt"
+    filNavn2 = "datafiler/trykk_og_temperaturlogg_rune_time.csv.txt"
+    Navn,Stasjon,Tid,Lufttemperatur,Lufttrykk = LagListe(filNavn1, 5)
+    Dato_og_tid,Tid_siden_start,Trykk_barometer,Trykk_absolutt, Temperatur = LagListe(filNavn2, 5)
 
-# print(LagListe("datafiler/temperatur_trykk_met_samme_rune_time_datasett.csv.txt",5))
-# print("")
-# print(LagListe("datafiler/trykk_og_temperaturlogg_rune_time.csv.txt",5))
+    NyTid = [datetime.strptime(dato, "%d.%m.%Y %H:%M") for dato in Tid]
+    
+    print(NyTid)
+    print(Lufttemperatur)
+    plt.plot(NyTid, Lufttemperatur, label = "Temperatur")
+    plt.plot(NyTid, Lufttrykk, label = "Trykk")
+    plt.xlabel = ('Tid')
+    plt.ylabel = ('Verdi')
+    plt.title('Temperatur og trykk fra første fil')
+    plt.legend()
+    plt.show()
